c139da36be5631f6c8c7531aa11da7b5de7a4203
vNicolini <valentin_n@hotmail.fr>
2024-01-17 16:04:19 +0100

    Update gafferTools_setup.bat

--- gafferTools_setup.bat
+++ gafferTools_setup.bat
@@ -1,19 +1,22 @@
 @echo off
-
 set SCRIPT_DIR=%cd%
 
-setx GAFFER_TOOLS "%SCRIPT_DIR%"
+setx GAFFER_TOOLS "%SCRIPT_DIR%" /M
+
+timeout /T 2 /nobreak >nul
+
+call set GAFFER_TOOLS=%SCRIPT_DIR%
 
 if not defined GAFFER_STARTUP_PATHS (
-    setx GAFFER_STARTUP_PATHS "%GAFFER_TOOLS%/startup"
+    setx GAFFER_STARTUP_PATHS "%GAFFER_TOOLS%/startup" /M
 ) else (
-    setx GAFFER_STARTUP_PATHS "%GAFFER_STARTUP_PATHS%;%GAFFER_TOOLS%/startup"
+    setx GAFFER_STARTUP_PATHS "%GAFFER_STARTUP_PATHS%;%GAFFER_TOOLS%/startup" /M
 )
 
 if not defined GAFFER_REFERENCE_PATHS (
-    setx GAFFER_REFERENCE_PATHS "%GAFFER_TOOLS%/resources/references"
+    setx GAFFER_REFERENCE_PATHS "%GAFFER_TOOLS%/resources/references" /M
 ) else (
-    setx GAFFER_REFERENCE_PATHS "%GAFFER_REFERENCE_PATHS%;%GAFFER_TOOLS%/resources/references"
+    setx GAFFER_REFERENCE_PATHS "%GAFFER_REFERENCE_PATHS%;%GAFFER_TOOLS%/resources/references" /M
 )
 
 rem check Gaffer paths are properly set up
