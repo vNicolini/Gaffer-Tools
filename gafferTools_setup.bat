5788f4c9f48867471f16d15473ae5833e16476c2
vNicolini <valentin_n@hotmail.fr>
2024-01-17 15:59:45 +0100

    Update gafferTools_setup.bat

--- gafferTools_setup.bat
+++ gafferTools_setup.bat
@@ -1,12 +1,20 @@
 @echo off
 
-for /f "delims=" %%I in ('cd /d "%~dp0" && echo %cd%') do set "SCRIPT_DIR=%%I"
+set SCRIPT_DIR=%cd%
 
 setx GAFFER_TOOLS "%SCRIPT_DIR%"
 
-setx GAFFER_STARTUP_PATHS "%GAFFER_STARTUP_PATHS%;%GAFFER_TOOLS%/startup"
+if not defined GAFFER_STARTUP_PATHS (
+    setx GAFFER_STARTUP_PATHS "%GAFFER_TOOLS%/startup"
+) else (
+    setx GAFFER_STARTUP_PATHS "%GAFFER_STARTUP_PATHS%;%GAFFER_TOOLS%/startup"
+)
 
-setx GAFFER_REFERENCE_PATHS "%GAFFER_REFERENCE_PATHS%;%GAFFER_TOOLS%/resources/references"
+if not defined GAFFER_REFERENCE_PATHS (
+    setx GAFFER_REFERENCE_PATHS "%GAFFER_TOOLS%/resources/references"
+) else (
+    setx GAFFER_REFERENCE_PATHS "%GAFFER_REFERENCE_PATHS%;%GAFFER_TOOLS%/resources/references"
+)
 
 rem check Gaffer paths are properly set up
 set | findstr "GAFFER" | find /i "GAFFER"
