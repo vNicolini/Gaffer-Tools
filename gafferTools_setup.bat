cc8c369c305f683e18d773b57a6b2976393e59c6
vNicolini <valentin_n@hotmail.fr>
2024-01-17 15:43:03 +0100

    edit win startup

--- gafferTools_setup.bat
+++ gafferTools_setup.bat
@@ -1,3 +1,5 @@
+@echo off
+
 @for /f "delims=" %%I in ('cd /d "%~dp0" && echo %cd%') do set SCRIPT_DIR=%%I
 
 setx GAFFER_TOOLS "%SCRIPT_DIR%"
@@ -9,4 +11,4 @@ setx GAFFER_REFERENCE_PATHS=%GAFFER_REFERENCE_PATHS%;%GAFFER_TOOLS/resources/ref
 rem check gaffer paths are properly setup
 set | findstr "GAFFER" | find /i "GAFFER"
 
-pause
+PAUSE
