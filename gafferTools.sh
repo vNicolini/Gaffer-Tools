dcdff90c7dd5dfd9256ab1ab04b5b0cd9cedfb4e
vNicolini <57097563+vNicolini@users.noreply.github.com>
2024-01-27 18:25:58 +0100

    Update linux setup scripts

--- gafferTools.sh
+++ gafferTools.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+# Get the current script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Set GAFFER_TOOLS environment variable
 export GAFFER_TOOLS=$SCRIPT_DIR
 
+# Update GAFFER_STARTUP_PATHS variable
 export GAFFER_STARTUP_PATHS=$GAFFER_STARTUP_PATHS:$GAFFER_TOOLS/startup
 
 export GAFFER_REFERENCE_PATHS=$GAFFER_REFERENCE_PATHS:$GAFFER_TOOLS/resources/references
@@ -10,5 +15,3 @@ exe() { echo "\$ ${@/eval/}" ; "$@" ; }
 
 # check gaffer paths are properly setup
 exe eval "export |grep GAFFER"
-
-read -n 1 -s -r -p "Press any key to continue..."
--- gafferTools_setup.sh
+++ gafferTools_setup.sh
@@ -1,14 +1,27 @@
+#!/bin/bash
+
+# Get the current script directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export GAFFER_TOOLS=$SCRIPT_DIR
+# Set GAFFER_TOOLS environment variable
+echo "export GAFFER_TOOLS=\"$SCRIPT_DIR\"" >> ~/.bashrc
 
-export GAFFER_STARTUP_PATHS=$GAFFER_STARTUP_PATHS:$GAFFER_TOOLS/startup
+# Update GAFFER_STARTUP_PATHS variable
+if [ -z "$GAFFER_STARTUP_PATHS" ]; then
+    echo "export GAFFER_STARTUP_PATHS=\"$SCRIPT_DIR/startup\"" >> ~/.bashrc
+else
+    echo "export GAFFER_STARTUP_PATHS=\"$GAFFER_STARTUP_PATHS:$SCRIPT_DIR/startup\"" >> ~/.bashrc
+fi
 
-export GAFFER_REFERENCE_PATHS=$GAFFER_REFERENCE_PATHS:$GAFFER_TOOLS/resources/references
+# Update GAFFER_REFERENCE_PATHS variable
+if [ -z "$GAFFER_REFERENCE_PATHS" ]; then
+    echo "export GAFFER_REFERENCE_PATHS=\"$SCRIPT_DIR/resources/references\"" >> ~/.bashrc
+else
+    echo "export GAFFER_REFERENCE_PATHS=\"$GAFFER_REFERENCE_PATHS:$SCRIPT_DIR/resources/references\"" >> ~/.bashrc
+fi
 
-exe() { echo "\$ ${@/eval/}" ; "$@" ; }
+# Source the updated .bashrc to apply changes
+source ~/.bashrc
 
-# check gaffer paths are properly setup
-exe eval "export |grep GAFFER"
-
+# Pause for user input
 read -n 1 -s -r -p "Press any key to continue..."
